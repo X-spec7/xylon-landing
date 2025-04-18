429367f4a916144e05f369949cb76e8e9b26cc6b
X-spec7 <mercury.spec77@gmail.com>
2025-04-18 23:39:30 +0200

    fix: fix layout style

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -1,13 +1,15 @@
 import type React from "react"
-import "./global.css"
+import { Footer, Header } from "@/components/Layout"
 import { Inter } from "next/font/google"
 import { dir } from "i18next"
-import { languages } from "../i18n/settings"
-import { useTranslation } from "../i18n"
+import "node_modules/react-modal-video/css/modal-video.css"
+
 import { Providers } from "./providers"
-import Header from "@/components/Layout/Header"
-import Footer from "@/components/Layout/Footer"
-import ScrollToTop from "@/components/Common/ScrollToTop"
+import { languages } from "../i18n/settings"
+// Import the non-hook version for metadata
+import { useTranslation } from "../i18n"
+
+import "../../styles/index.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -25,16 +27,23 @@ export default async function RootLayout({
   const { t } = await useTranslation(lng, "common")
 
   return (
-    <html lang={lng} dir={dir(lng)}>
+    <html suppressHydrationWarning lang={lng} dir={dir(lng)}>
       <head>
         <title>{t("app.title")}</title>
       </head>
-      <body className={inter.className}>
+
+      <body className={`bg-[#FCFCFC] ${inter.className}`}>
         <Providers>
           <Header lng={lng} />
-          <main>{children}</main>
-          <Footer lng={lng} />
-          <ScrollToTop />
+          <div
+            style={{
+              backgroundImage: "url('/images/bg.jpg')",
+            }}
+            className="z-10 w-full h-screen bg-cover bg-center overflow-auto no-scrollbar"
+          >
+            {children}
+            <Footer lng={lng} />
+          </div>
         </Providers>
       </body>
     </html>
